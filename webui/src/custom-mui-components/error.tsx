4cbaef4ed133932598130a1a6a965fb4cb4e6235
Sean Hellum <seanhellum45@gmail.com>
2020-04-07 02:43:22 +0000

    fix

--- webui/src/custom-mui-components/error.tsx
+++ webui/src/custom-mui-components/error.tsx
@@ -12,37 +12,51 @@ import * as React from "react";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
+import { Theme, WithStyles, createStyles, withStyles, Popover } from "@material-ui/core";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
-export default function ErrorDialog(props: { title: React.ReactNode; message: React.ReactNode; }) {
-  const [open, setOpen] = React.useState(true);
+export default class ErrorDialog extends React.Component<ErrorDialog.Props, ErrorDialog.State> {
+    constructor(props: Readonly<ErrorDialog.Props>) {
+        super(props);
+        this.state = {};
+    }
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    handleClose(this: any) {
+        this.setState({ anchor: null })
+    }
 
-  return (
-    <div>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{props.title}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            {props.message}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Ok
+    render() {
+        return <div>
+            <Dialog
+                open={open}
+                onClose={() => this.setState({ anchor: null })}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{this.props.title}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        {this.props.message}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.handleClose} color="primary">
+                        Ok
           </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
+                </DialogActions>
+            </Dialog>
+        </div>;
+    }
+}
+
+export namespace ErrorDialog {
+    export interface Props {
+        title: React.ReactNode,
+        message: React.ReactNode
+    }
+    export interface State {
+        anchor?: Element | null;
+    }
 }
